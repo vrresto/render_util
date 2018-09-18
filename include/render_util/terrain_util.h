1317047bae0ecf469cfa439083f807476971dba7
Jan Lepper <jan_lepper@gmx.de>
2018-09-18 10:30:55 +0200

    add struct TerrainRenderer; createTerrainRenderer()

---
+++ include/render_util/terrain_util.h
@@ -0,0 +1,44 @@
+/**
+ *    Rendering utilities
+ *    Copyright (C) 2018  Jan Lepper
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef RENDER_UTIL_TERRAIN_UTIL_H
+#define RENDER_UTIL_TERRAIN_UTIL_H
+
+#include <render_util/shader.h>
+#include <render_util/terrain_base.h>
+#include <render_util/texture_manager.h>
+
+#include <string>
+
+namespace render_util
+{
+
+
+struct TerrainRenderer
+{
+  ShaderProgramPtr m_program;
+  std::shared_ptr<TerrainBase> m_terrain;
+};
+
+TerrainRenderer createTerrainRenderer(TextureManager &tex_mgr, bool use_lod, const std::string &shader_path);
+
+
+} // namespace render_util
+
+
+#endif
---
+++ src/terrain_util.cpp
@@ -0,0 +1,77 @@
+/**
+ *    Rendering utilities
+ *    Copyright (C) 2018  Jan Lepper
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <render_util/terrain_util.h>
+#include <render_util/shader_util.h>
+#include <render_util/terrain.h>
+#include <render_util/terrain_cdlod.h>
+
+#include <gl_wrapper/gl_functions.h>
+
+using namespace render_util;
+using namespace std;
+using namespace gl_wrapper::gl_functions;
+
+namespace
+{
+
+
+ShaderProgramPtr createTerrainProgram(const TextureManager &tex_mgr, bool cdlod, const string &shader_path)
+{
+  const string name = cdlod ? "terrain_cdlod" : "terrain";
+
+  ShaderProgramPtr terrain_program;
+
+  CHECK_GL_ERROR();
+
+  map<unsigned int, string> attribute_locations;
+
+  if (cdlod)
+    attribute_locations = { { 4, "attrib_pos" } };
+
+  terrain_program = createShaderProgram(name, tex_mgr, shader_path, attribute_locations);
+
+  CHECK_GL_ERROR();
+
+  return terrain_program;
+}
+
+
+} // namespace
+
+namespace render_util
+{
+
+
+TerrainRenderer createTerrainRenderer(TextureManager &tex_mgr, bool use_lod, const string &shader_path)
+{
+  TerrainRenderer r;
+
+  r.m_program = createTerrainProgram(tex_mgr, use_lod, shader_path);
+  if (use_lod)
+    r.m_terrain = g_terrain_cdlod_factory();
+  else
+    r.m_terrain = g_terrain_factory();
+
+  r.m_terrain->setTextureManager(&tex_mgr);
+
+  return r;
+}
+
+
+} // namespace terrain_util
