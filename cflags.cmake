69247dff43f95eaa4d5da30c5bbff5b472e57cc9
Jan Lepper <jan_lepper@gmx.de>
2019-02-23 22:07:55 +0100

    turn on optimisations depending on 'enable_debug' variable

--- cflags.cmake
+++ cflags.cmake
@@ -1,13 +1,22 @@
 # Optimisation
-# set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=haswell")
-# set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -msse3")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mssse3")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfpmath=sse")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -ffast-math") # makes std::sort<float>() crash
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -ftree-vectorize")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O2")
-# set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fno-omit-frame-pointer")
-# set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fno-strict-aliasing")
+if(NOT enable_debug)
+  # set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=haswell")
+  # set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -msse3")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mssse3")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfpmath=sse")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -ffast-math") # makes std::sort<float>() crash
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -ftree-vectorize")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O2")
+
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DGLM_FORCE_DEFAULT_ALIGNED_GENTYPES")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DGLM_FORCE_INTRINSICS")
+  # set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DGLM_FORCE_SSE3")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DGLM_FORCE_SSSE3")
+  # set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DGLM_FORCE_AVX2")
+else()
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fno-omit-frame-pointer")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fno-strict-aliasing")
+endif()
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wfatal-errors")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g")
@@ -15,11 +24,6 @@ set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pipe")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wreturn-type -Werror -Wmissing-field-initializers")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -D_REENTRANT")
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DGLM_FORCE_DEFAULT_ALIGNED_GENTYPES")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DGLM_FORCE_INTRINSICS")
-# set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DGLM_FORCE_SSE3")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DGLM_FORCE_SSSE3")
-# set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DGLM_FORCE_AVX2")
 
 
 # -fdata-sections -ffunction-sections
