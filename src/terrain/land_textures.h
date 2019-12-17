7c108429cbc4f88d5967a7dd80e7b3b82e4ac13d
Jan Lepper <jan_lepper@gmx.de>
2019-12-17 02:35:00 +0100

    rename TerrainTextures => LandTextures and move into land_textures.cpp

---
+++ src/terrain/land_textures.h
@@ -0,0 +1,68 @@
+/**
+ *    Rendering utilities
+ *    Copyright (C) 2019 Jan Lepper
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef _RENDER_UTIL_TERRAIN_LAND_TEXTURES_H
+#define _RENDER_UTIL_TERRAIN_LAND_TEXTURES_H
+
+#include <render_util/texture_manager.h>
+#include <render_util/texunits.h>
+#include <render_util/shader.h>
+#include <render_util/image.h>
+#include <render_util/terrain_base.h>
+
+#include <glm/glm.hpp>
+
+namespace render_util::terrain
+{
+
+
+class LandTextures
+{
+  const TextureManager &m_texture_manager;
+  glm::ivec2 m_type_map_size = glm::ivec2(0);
+  TexturePtr m_type_map_texture;
+  TexturePtr m_type_map_texture_nm;
+  ShaderParameters m_shader_params;
+  std::array<TexturePtr, MAX_TERRAIN_TEXUNITS> m_textures;
+  std::array<TexturePtr, MAX_TERRAIN_TEXUNITS> m_textures_nm;
+  bool m_enable_normal_maps = false;
+
+public:
+  static constexpr float MAX_TEXTURE_SCALE = 8;
+
+  LandTextures(const TextureManager &texture_manager,
+                  std::vector<ImageRGBA::Ptr> &textures,
+                  std::vector<ImageRGB::Ptr> &textures_nm,
+                  const std::vector<float> &texture_scale,
+                  TerrainBase::TypeMap::ConstPtr type_map);
+
+  const ShaderParameters &getShaderParameters() { return m_shader_params; }
+
+  void setTextures(const std::vector<ImageRGBA::ConstPtr> &textures,
+                          const std::vector<float> &texture_scale,
+                          TerrainBase::TypeMap::ConstPtr type_map);
+
+  void bind(TextureManager&);
+  void unbind(TextureManager&);
+  void setUniforms(ShaderProgramPtr program);
+};
+
+
+}
+
+#endif
--- src/terrain/terrain_cdlod.cpp
+++ src/terrain/terrain_cdlod.cpp
@@ -24,6 +24,7 @@
 
 #include "grid_mesh.h"
 #include "terrain_cdlod_base.h"
+#include "land_textures.h"
 #include "vao.h"
 #include <render_util/terrain_cdlod.h>
 #include <render_util/texture_manager.h>
@@ -39,8 +40,6 @@
 #include <render_util/globals.h>
 #include <block_allocator.h>
 
-#include <set>
-#include <deque>
 #include <array>
 #include <vector>
 #include <iostream>
@@ -50,6 +49,9 @@
 #include <GL/gl.h>
 #include <render_util/gl_binding/gl_functions.h>
 #include <log.h>
+
+
+using namespace render_util::terrain;
 
 using namespace render_util::gl_binding;
 using namespace glm;
@@ -74,326 +76,6 @@ class Material;
 
 using MaterialID = render_util::TerrainBase::MaterialID;
 using BoundingBox = render_util::Box;
-
-
-template <class T>
-void createTextureArrays(std::vector<typename T::Ptr> &textures_in,
-    const std::vector<float> &texture_scale_in,
-    TerrainBase::TypeMap::ConstPtr type_map_in,
-    double max_texture_scale,
-    std::array<TexturePtr, render_util::MAX_TERRAIN_TEXUNITS> &arrays_out,
-    TexturePtr &type_map_texture_out)
-{
-  LOG_TRACE<<"createTextureArrays()"<<endl;
-
-  using namespace glm;
-  using namespace std;
-  using namespace render_util;
-  using TextureArray = vector<typename T::ConstPtr>;
-
-  std::array<TextureArray, MAX_TERRAIN_TEXUNITS> texture_arrays;
-  map<unsigned, glm::uvec3> mapping;
-
-  std::set<size_t> all_texture_sizes;
-  for (auto texture : textures_in)
-  {
-    if (!texture)
-      continue;
-    assert(texture->w() == texture->h());
-    all_texture_sizes.insert(texture->w());
-  }
-
-  std::deque<size_t> texture_sizes;
-  for (auto size : all_texture_sizes)
-    texture_sizes.push_back(size);
-
-  while (texture_sizes.size() > MAX_TERRAIN_TEXUNITS)
-    texture_sizes.pop_front();
-
-  auto smallest_size = texture_sizes.front();
-
-  std::unordered_map<int, int> array_index_for_size;
-  for (size_t i = 0; i < texture_sizes.size(); i++)
-    array_index_for_size[texture_sizes.at(i)] = i;
-
-  for (int i = 0; i < textures_in.size(); i++)
-  {
-    float scale = texture_scale_in.at(i);
-    assert(scale != 0);
-    assert(fract(scale) == 0);
-    assert(scale <= max_texture_scale);
-    scale += 128;
-    assert(scale >= 0);
-    assert(scale <= 255);
-
-    auto image = textures_in.at(i);
-    if (!image)
-      continue;
-
-    while (image->w() < smallest_size)
-    {
-//       auto biggest_size = texture_sizes.back();
-//       LOG_INFO<<"image->w(): "<<image->w()<<", smallest_size: "
-//         <<smallest_size<<", biggest_size: "<<biggest_size<<endl;
-      image = render_util::upSample(image, 2);
-    }
-
-    auto index = array_index_for_size.at(image->w());
-
-    texture_arrays.at(index).push_back(image);
-    mapping.insert(make_pair(i, glm::uvec3{index, texture_arrays[index].size()-1, scale}));
-
-    textures_in.at(i).reset();
-  }
-
-  auto type_map = make_shared<ImageRGBA>(type_map_in->getSize());
-
-  for (int y = 0; y < type_map->h(); y++)
-  {
-    for (int x = 0; x < type_map->w(); x++)
-    {
-      unsigned int orig_index = type_map_in->get(x,y) & 0x1F;
-
-      uvec3 new_index(0);
-
-      auto it = mapping.find(orig_index);
-      if (it != mapping.end())
-      {
-        new_index = it->second;
-      }
-      else
-      {
-        for (int i = 0; i < 4; i++)
-        {
-          auto it = mapping.find(orig_index - (orig_index % 4) + i);
-          if (it != mapping.end())
-          {
-            new_index = it->second;
-            break;
-          }
-          else
-          {
-            new_index.x = 255;
-          }
-        }
-      }
-
-      assert(new_index.y <= 0x1F+1);
-      type_map->at(x,y,0) = new_index.x;
-      type_map->at(x,y,1) = new_index.y;
-      type_map->at(x,y,2) = new_index.z;
-      type_map->at(x,y,3) = 255;
-    }
-  }
-
-  {
-    TexturePtr t = render_util::createTexture(type_map, false);
-    TextureParameters<int> params;
-    params.set(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-    params.set(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-    params.set(GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    params.set(GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    params.apply(t);
-    type_map_texture_out = t;
-  }
-
-  for (int i = 0; i < texture_arrays.size(); i++)
-  {
-    CHECK_GL_ERROR();
-
-    auto &textures = texture_arrays.at(i);
-    if (textures.empty())
-      continue;
-
-    LOG_TRACE<<"array: "<<i<<endl;
-    arrays_out.at(i) = render_util::createTextureArray<T>(textures);
-    textures.clear();
-
-    CHECK_GL_ERROR();
-  }
-}
-
-
-class TerrainTextures
-{
-  const TextureManager &m_texture_manager;
-  glm::ivec2 m_type_map_size = glm::ivec2(0);
-  TexturePtr m_type_map_texture;
-  TexturePtr m_type_map_texture_nm;
-  ShaderParameters m_shader_params;
-  std::array<TexturePtr, render_util::MAX_TERRAIN_TEXUNITS> m_textures;
-  std::array<TexturePtr, render_util::MAX_TERRAIN_TEXUNITS> m_textures_nm;
-  bool m_enable_normal_maps = false;
-
-public:
-  static constexpr float MAX_TERRAIN_TEXTURE_SCALE = 8;
-
-  TerrainTextures(const TextureManager &texture_manager,
-                  std::vector<ImageRGBA::Ptr> &textures,
-                  std::vector<ImageRGB::Ptr> &textures_nm,
-                  const std::vector<float> &texture_scale,
-                  TerrainBase::TypeMap::ConstPtr type_map);
-
-  const ShaderParameters &getShaderParameters() { return m_shader_params; }
-
-  void setTextures(const std::vector<ImageRGBA::ConstPtr> &textures,
-                          const std::vector<float> &texture_scale,
-                          TerrainBase::TypeMap::ConstPtr type_map);
-
-  void bind(TextureManager&);
-  void unbind(TextureManager&);
-  void setUniforms(ShaderProgramPtr program);
-};
-
-
-TerrainTextures::TerrainTextures(const TextureManager &texture_manager,
-                                      std::vector<ImageRGBA::Ptr> &textures,
-                                      std::vector<ImageRGB::Ptr> &textures_nm,
-                                      const std::vector<float> &texture_scale,
-                                      TerrainBase::TypeMap::ConstPtr type_map_) :
-  m_texture_manager(texture_manager),
-  m_type_map_size(type_map_->getSize())
-{
-  using namespace glm;
-  using namespace std;
-  using namespace render_util;
-  using TextureArray = vector<ImageRGBA::ConstPtr>;
-
-  assert(textures.size() == texture_scale.size());
-
-  m_enable_normal_maps = !textures_nm.empty();
-
-  createTextureArrays<ImageRGBA>(textures, texture_scale, type_map_, MAX_TERRAIN_TEXTURE_SCALE,
-                      m_textures, m_type_map_texture);
-
-  if (m_enable_normal_maps)
-  {
-    m_shader_params.set("enable_terrain_detail_nm", true);
-    assert(textures.size() == textures_nm.size());
-
-    createTextureArrays<ImageRGB>(textures_nm, texture_scale, type_map_, MAX_TERRAIN_TEXTURE_SCALE,
-                                  m_textures_nm, m_type_map_texture_nm);
-  }
-
-  for (int i = 0; i < m_textures.size(); i++)
-  {
-    CHECK_GL_ERROR();
-
-    if (!m_textures.at(i))
-      continue;
-
-    m_shader_params.set(string( "enable_terrain") + to_string(i), true);
-  }
-
-  for (int i = 0; i < m_textures_nm.size(); i++)
-  {
-    CHECK_GL_ERROR();
-
-    if (!m_textures_nm.at(i))
-      continue;
-
-    m_shader_params.set(string( "enable_terrain_detail_nm") + to_string(i), true);
-  }
-}
-
-
-void TerrainTextures::bind(TextureManager &tm)
-{
-  using namespace render_util;
-
-  tm.bind(TEXUNIT_TYPE_MAP, m_type_map_texture);
-
-  for (int i = 0; i < m_textures.size(); i++)
-  {
-    CHECK_GL_ERROR();
-
-    auto textures = m_textures.at(i);
-    if (!textures)
-      continue;
-
-    auto texunit = TEXUNIT_TERRAIN + i;
-    assert(texunit < TEXUNIT_NUM);
-
-    tm.bind(texunit, textures);
-
-    CHECK_GL_ERROR();
-  }
-
-  if (m_enable_normal_maps)
-  {
-    tm.bind(TEXUNIT_TYPE_MAP_NORMALS, m_type_map_texture_nm);
-
-    for (int i = 0; i < m_textures_nm.size(); i++)
-    {
-      CHECK_GL_ERROR();
-
-      auto textures = m_textures_nm.at(i);
-      if (!textures)
-        continue;
-
-      auto texunit = TEXUNIT_TERRAIN_DETAIL_NM0 + i;
-      assert(texunit < TEXUNIT_NUM);
-
-      tm.bind(texunit, textures);
-
-      CHECK_GL_ERROR();
-    }
-  }
-}
-
-
-void TerrainTextures::unbind(TextureManager &tm)
-{
-  LOG_TRACE<<endl;
-
-  using namespace render_util;
-
-  tm.unbind(TEXUNIT_TYPE_MAP, m_type_map_texture->getTarget());
-
-  for (int i = 0; i < m_textures.size(); i++)
-  {
-    CHECK_GL_ERROR();
-
-    auto texture = m_textures.at(i);
-    if (!texture)
-      continue;
-
-    auto texunit = TEXUNIT_TERRAIN + i;
-    assert(texunit < TEXUNIT_NUM);
-
-    tm.unbind(texunit, texture->getTarget());
-
-    CHECK_GL_ERROR();
-  }
-
-  if (m_enable_normal_maps)
-  {
-    tm.unbind(TEXUNIT_TYPE_MAP_NORMALS, m_type_map_texture_nm->getTarget());
-
-    for (int i = 0; i < m_textures_nm.size(); i++)
-    {
-      CHECK_GL_ERROR();
-
-      auto texture = m_textures_nm.at(i);
-      if (!texture)
-        continue;
-
-      auto texunit = TEXUNIT_TERRAIN_DETAIL_NM0 + i;
-      assert(texunit < TEXUNIT_NUM);
-
-      tm.unbind(texunit, texture->getTarget());
-
-      CHECK_GL_ERROR();
-    }
-  }
-}
-
-
-void TerrainTextures::setUniforms(ShaderProgramPtr program)
-{
-  assert(m_type_map_size != glm::ivec2(0));
-  program->setUniform("typeMapSize", m_type_map_size);
-}
 
 
 struct DetailOption
@@ -771,7 +453,7 @@ class TerrainCDLOD : public TerrainCDLODBase
 
   std::vector<TerrainLayer> m_layers;
   std::unordered_map<unsigned int, std::unique_ptr<Material>> materials;
-  std::unique_ptr<TerrainTextures> m_terrain_textures;
+  std::unique_ptr<LandTextures> m_terrain_textures;
   render_util::ShaderParameters m_shader_params;
   std::string m_program_name;
 
@@ -878,7 +560,7 @@ void TerrainCDLOD::setUniforms(ShaderProgramPtr program)
   program->setUniformi("terrain.mesh_resolution_m", GRID_RESOLUTION_M);
 
   program->setUniformi("terrain.tile_size_m", TILE_SIZE_M);
-  program->setUniform("terrain.max_texture_scale", TerrainTextures::MAX_TERRAIN_TEXTURE_SCALE);
+  program->setUniform("terrain.max_texture_scale", LandTextures::MAX_TEXTURE_SCALE);
 
   assert(m_terrain_textures);
   m_terrain_textures->setUniforms(program);
@@ -994,7 +676,7 @@ void TerrainCDLOD::build(BuildParameters &params)
   CHECK_GL_ERROR();
 
   m_terrain_textures =
-    std::make_unique<TerrainTextures>(texture_manager, params.textures,
+    std::make_unique<LandTextures>(texture_manager, params.textures,
                                       params.textures_nm, params.texture_scale, params.type_map);
 
   auto hm_image = params.map;
