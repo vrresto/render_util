282260a0050c71ec3bec00a54b5d979c9df69a93
Jan Lepper <jan_lepper@gmx.de>
2020-07-01 22:07:05 +0200

    cleanup

--- src/water.cpp
+++ src/water.cpp
@@ -31,94 +31,12 @@
 #include "GL/gl.h"
 
 
-using Clock = std::chrono::steady_clock;
-
-using namespace std;
-
-
-
-namespace
-{
-
-
-constexpr int NUM_LAYERS = 2;
-
-struct Layer
-{
-  const int step_duration_microseconds = 0;
-  int current_step = 0;
-  std::chrono::milliseconds delta_last_frame;
-
-  Clock::time_point last_step_time;
-  Clock::time_point next_step_time;
-
-  Layer(float fps) : step_duration_microseconds(1000 * (1000.f / fps))
-  {
-    last_step_time = Clock::now();
-    next_step_time = Clock::now() + std::chrono::microseconds(step_duration_microseconds);
-  }
-
-  float getFrameDelta() const
-  {
-    return  (float)delta_last_frame.count() / ((float)step_duration_microseconds / 1000.f);
-  }
-};
-
-
-} // namespace
-
-
 namespace render_util
 {
 
 
 struct WaterAnimation::Private
 {
-  array<Layer, NUM_LAYERS> layers { 2, 6 };
-
-  int num_animation_steps = 0;
-
-  const Layer &getLayer(size_t index) { return layers.at(index); }
-
-  void createTextures(MapTextures *map_textures,
-                      const vector<ImageRGBA::ConstPtr> &normal_maps,
-                      const vector<ImageGreyScale::ConstPtr> &foam_masks)
-  {
-    assert(foam_masks.size() == normal_maps.size());
-
-    num_animation_steps = normal_maps.size();
-
-    assert(0);
-    abort();
-//     map_textures->setTextureArray(TEXUNIT_WATER_NORMAL_MAP, normal_maps);
-//     map_textures->setTextureArray(TEXUNIT_FOAM_MASK, foam_masks);
-
-  }
-
-
-  void update()
-  {
-    if (!num_animation_steps)
-      return;
-
-    Clock::time_point now = Clock::now();
-
-    for (Layer &l : layers)
-    {
-      if (now >= l.next_step_time)
-      {
-        l.last_step_time = l.next_step_time;
-        l.next_step_time += std::chrono::microseconds(l.step_duration_microseconds);
-        l.current_step++;
-        l.current_step %= num_animation_steps;
-      }
-
-      l.delta_last_frame =
-        std::chrono::duration_cast<std::chrono::milliseconds>(now - l.last_step_time);
-    }
-
-  }
-
 };
 
 
@@ -130,33 +48,22 @@ WaterAnimation::~WaterAnimation()
 }
 
 void WaterAnimation::createTextures(MapTextures *map_textures,
-                                    const vector<ImageRGBA::ConstPtr> &normal_maps,
-                                    const vector<ImageGreyScale::ConstPtr> &foam_masks)
+                                    const std::vector<ImageRGBA::ConstPtr> &normal_maps,
+                                    const std::vector<ImageGreyScale::ConstPtr> &foam_masks)
 {
-  p->createTextures(map_textures, normal_maps, foam_masks);
 }
 
 void WaterAnimation::update()
 {
-  p->update();
 }
 
 void WaterAnimation::updateUniforms(ShaderProgramPtr program)
 {
-  program->setUniformi("water_animation_num_frames", p->num_animation_steps);
-
-  for (int i = 0; i < NUM_LAYERS; i++)
-  {
-    string prefix = string("water_animation_params[") + to_string(i) + "].";
-
-    program->setUniformi(prefix + "pos", p->getLayer(i).current_step);
-    program->setUniform<float>(prefix + "frame_delta", p->getLayer(i).getFrameDelta());
-  }
 }
 
 bool WaterAnimation::isEmpty()
 {
-  return !p->num_animation_steps;
+  abort();
 }
 
 
